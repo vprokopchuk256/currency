1e3f120d70457bb5f197479797f22357f00ec7c9
Valeriy Prokopchuk <vprokopchuk@gmail.com>
2017-04-25 17:20:06 -0500

    graph specs

--- src/Graph/Graph.hs
+++ src/Graph/Graph.hs
@@ -1,27 +1,39 @@
 module Graph.Graph
-(
-  Graph
-, empty
-, size
-, vertices
-, edges
-, addEdge
-) where
+    ( empty
+    , size
+    , vertices
+    , edges
+    , addEdge
+    ) where
 
-import qualified Data.List as List
 import qualified Data.Map as Map
+import qualified Data.List as List
+
 import qualified Graph.Edge as Edge
 
-type Graph = Map.Map String [Edge.Edge]
+type VertexName = String
+newtype Graph = Graph
+    { getMap :: Map.Map VertexName [Edge.Edge] }
 
-empty = Map.empty
-size = Map.size
-vertices = Map.keys
+empty :: Graph
+empty = Graph Map.empty
 
-addEdge :: String -> String -> Float -> Graph -> Graph
-addEdge from to rate graph =
-  let edge = Edge.new from to rate
-  in Map.insertWith (++) from [edge] graph
+size :: Graph -> Int
+size graph = Map.size $ getMap graph
+
+vertices :: Graph -> [VertexName]
+vertices graph = Map.keys $ getMap graph
 
 edges :: Graph -> [Edge.Edge]
-edges graph = List.concat $ Map.elems graph
+edges graph = List.concat $ Map.elems $ getMap graph
+
+addEdge :: VertexName -> VertexName -> Float -> Graph -> Graph
+addEdge from to rate (Graph map) =
+  let
+    edge = Edge.new from to rate
+    insert = Map.insertWith (++)
+  in
+    Graph $ (insert from [edge]) . (insert to []) $ map
+
+instance Show Graph where
+  show graph = List.intercalate ", " $ map show $ edges graph
---
+++ test/Graph/GraphSpec.hs
@@ -0,0 +1,53 @@
+module Graph.GraphSpec (main, spec) where
+
+import Test.Hspec
+
+import qualified Data.List as List
+
+import qualified Graph.Edge as Edge
+import qualified Graph.Graph as Graph
+
+main :: IO ()
+main = hspec spec
+
+spec :: Spec
+spec = do
+  describe "empty graph" $ do
+    let graph = Graph.empty
+
+    it "has zero size" $ do
+      (Graph.size graph) `shouldBe` 0
+
+    it "has empty vertices list" $ do
+      (Graph.vertices graph) `shouldBe` []
+
+    it "has empty edges list" $ do
+      (Graph.edges graph) `shouldBe` []
+
+    it "has empty description" $ do
+      (show graph) `shouldBe` ""
+
+  describe "graph with one edge" $ do
+    let graph = Graph.addEdge "USD" "CHF" 10.0 Graph.empty
+
+    it "has size equals 2" $ do
+      (Graph.size graph) `shouldBe` 2
+
+    it "has vertices list that includes both sides of the edge" $ do
+      (List.sort (Graph.vertices graph)) `shouldBe` ["CHF", "USD"]
+
+    it "has edges list with one just inserted edge" $ do
+      (Graph.edges graph) `shouldBe` [Edge.new "USD" "CHF" 10.0]
+
+  describe "with two connected edges" $ do
+    let graph = (Graph.addEdge "USD" "CHF" 10.0) .
+                (Graph.addEdge "CHF" "EUR" 20.0) $ Graph.empty
+
+    it "has size equals 3" $ do
+      (Graph.size graph) `shouldBe` 3
+
+    it "has vertices list that includes all sides of the edges" $ do
+      (List.sort (Graph.vertices graph)) `shouldBe` ["CHF", "EUR", "USD"]
+
+    it "has edges list with all inserted edges" $ do
+      (Graph.edges graph) `shouldBe` [(Edge.new "CHF" "EUR" 20.0), (Edge.new "USD" "CHF" 10.0)]
