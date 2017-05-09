75c3017e787469fa4fa7c216c7c08410fd081cbc
Valeriy Prokopchuk <vprokopchuk@gmail.com>
2017-05-08 23:36:13 -0500

    relaxable with nullable tree nodes

--- src/Graph/Relaxable.hs
+++ src/Graph/Relaxable.hs
@@ -3,10 +3,14 @@ module Graph.Relaxable ( Relaxable(..) ) where
 import qualified Data.HashMap.Strict as Map
 import Data.List(intercalate)
 
-data Relaxable a = Tree (Map.HashMap a (a, Float)) | Cycle [a] Float deriving (Eq)
+type Entry a = Maybe (Maybe a, Float)
+data Relaxable a = Tree (Map.HashMap a (Entry a)) | Cycle [a] Float deriving (Eq)
 
 instance (Show a) => Show (Relaxable a) where
     show (Tree mp) = intercalate ", " $ map f $ Map.toList mp
-      where f (to, (from, dist)) = intercalate " <- " [(show to), (show dist), (show from)]
-    show (Cycle [] p) = ""
+      where
+        f (to, Nothing) = show to
+        f (to, Just (Nothing, dist)) = intercalate " <- " [(show to), (show dist)]
+        f (to, Just (Just from, dist)) = intercalate " <- " [(show to), (show dist), (show from)]
+    show (Cycle [] _) = ""
     show (Cycle ns p) = (intercalate " <- " $ map show ns) ++ " (" ++ show p ++ ")"
--- test/Graph/RelaxableSpec.hs
+++ test/Graph/RelaxableSpec.hs
@@ -3,6 +3,8 @@ module Graph.RelaxableSpec (main, spec) where
 import Test.Hspec
 
 import qualified Data.HashMap.Strict as Map
+
+import qualified Graph.Graph as Graph
 import qualified Graph.Relaxable as Relaxable
 
 main :: IO ()
@@ -14,20 +16,33 @@ spec = do
         describe "tree" $ do
             context "when empty" $ do
                 let tree = show $ (Relaxable.Tree Map.empty :: Relaxable.Relaxable String)
+
                 it "it returns empty string" $ do
                     tree `shouldBe` ""
 
-            context "when has nodes with distance info" $ do
-                let tree = show $ Relaxable.Tree (Map.fromList [("A", ("B", 123.45))])
+            context "when has nodes without previous info and distance" $ do
+                let tree = show $ Relaxable.Tree (Map.fromList [("A", Nothing)])
+
+                it "it returns string with node information" $ do
+                    tree `shouldBe` "\"A\""
+
+            context "when has nodes with previous not and distance info" $ do
+                let tree = show $ Relaxable.Tree (Map.fromList [("A", (Just ((Just "B"), 123.45)))])
+
                 it "it returns string with routing information" $ do
                     tree `shouldBe` "\"A\" <- 123.45 <- \"B\""
+
         describe "cycle" $ do
             context "when empty" $ do
                 let cycle = show $ (Relaxable.Cycle [] 0.0 :: Relaxable.Relaxable String)
+
                 it "it returns empty string" $ do
                     cycle `shouldBe` ""
+
             context "when has nodes" $ do
                 let cycle = show $ (Relaxable.Cycle ["A", "B"] 0.45 :: Relaxable.Relaxable String)
+
                 it "it returns string with routing information" $ do
                     cycle `shouldBe` "\"A\" <- \"B\" (0.45)"
+
 
