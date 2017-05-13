5c0d8c5384028254da0594c08652ee6c145dd076
Valeriy Prokopchuk <vprokopchuk@gmail.com>
2017-05-13 16:37:06 -0500

    [wip] relax simplification

--- src/Graph/Relaxable.hs
+++ src/Graph/Relaxable.hs
@@ -42,7 +42,7 @@ relax' (Tree mp) (Edge from to _ weight) = ins (mp ! from) (mp ! to)
         isRelaxed = weightN < weightT
 
 relax :: (Eq a, Hashable a) => Relaxable a -> [Edge a] -> Relaxable a
-relax t es = foldl relax' t es
+relax = foldl relax'
 
 tryRelax :: (Eq a, Hashable a) => Edge a -> Relaxable a -> Relaxable a
 tryRelax (Edge from to _ weight) (Tree mp) = ins (mp ! from) (mp ! to)
